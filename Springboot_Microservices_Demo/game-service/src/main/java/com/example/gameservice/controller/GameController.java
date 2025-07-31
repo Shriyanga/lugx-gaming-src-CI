b06ab4e95d343008b895a247775c9e4f4661eba5
Shriyanga <shriyangaherath@gmail.com>
2025-07-31 14:35:58 +0530

    2nd commit

--- Springboot_Microservices_Demo/game-service/src/main/java/com/example/gameservice/controller/GameController.java
+++ Springboot_Microservices_Demo/game-service/src/main/java/com/example/gameservice/controller/GameController.java
@@ -24,6 +24,7 @@ public class GameController {
     @PostMapping
     public ResponseEntity<?> saveGame(@RequestBody GameDTO gameDTO) {
         GameDTO dto = gameService.saveGame(gameDTO);
+        System.out.println("hello");
         return new ResponseEntity<>(new StandardResponse<>(201, "Success", dto), HttpStatus.CREATED);
     }
 
