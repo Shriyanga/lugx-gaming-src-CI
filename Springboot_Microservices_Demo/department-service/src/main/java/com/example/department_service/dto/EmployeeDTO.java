8d4d8f19a81e4b5df8a46f1a6f1c32ba5e3a8d1f
Shriyanga <150236006+Shriyanga@users.noreply.github.com>
2025-08-03 07:43:46 +0530

    Delete Springboot_Microservices_Demo/department-service directory

--- Springboot_Microservices_Demo/department-service/src/main/java/com/example/department_service/dto/EmployeeDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.department_service.dto;
-
-/**
- * @author: Thrimal Avishka
- * @since: 2025-02-19
- */
-
-public record EmployeeDTO(Long id, Long departmentId, String name) {
-}
