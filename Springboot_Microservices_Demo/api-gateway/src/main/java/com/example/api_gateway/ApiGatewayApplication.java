8dd024ac14e907388a40a1967dee416dd14d036d
Shriyanga <shriyangaherath@gmail.com>
2025-08-14 16:20:59 +0530

    4rd commit

--- Springboot_Microservices_Demo/api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
+++ Springboot_Microservices_Demo/api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * @author: Thrimal Avishka
- * @since: 2025-02-24
- */
 
 @SpringBootApplication
 @EnableDiscoveryClient
